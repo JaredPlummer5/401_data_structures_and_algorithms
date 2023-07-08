62455e25e80390aa4bd54c0ab721728023046887
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-07-08 14:37:07 -0500

    Challenge 1 Complete

--- c-sharp/CodeChallenge1/ArrayReverse.cs
+++ c-sharp/CodeChallenge1/ArrayReverse.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 namespace CodeChallenges
 {
-    public class CodeChallenge1
+  public class CodeChallenge1
+  {
+    public static int[] ReverseArray(int[] array)
     {
-        public static int[] ReverseArray(int[] array)
-        {
-
-            return array;
-        }
+      //Console.WriteLine("Hello WOrld");
+      int[] newArray = new int[array.Length];
+      int arrayLength = array.Length;
+      for (int i = arrayLength; i > 0; i--)
+      {
+        newArray[arrayLength - i] = array[i - 1];
+      }
+      return newArray;
     }
+  }
 }
