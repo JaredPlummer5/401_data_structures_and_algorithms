a2fd4128724898b19edea46fbcbfe36b3dd50fc6
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-08-02 16:18:03 -0500

    Chal 13 complete

--- c-sharp/CodeChallenge13/StackQueueBrackets.cs
+++ c-sharp/CodeChallenge13/StackQueueBrackets.cs
@@ -1,10 +1,40 @@
-namespace CodeChallenges 
+using System.Collections;
+
+namespace CodeChallenges
 {
-    public class CodeChallenge13
+  public class CodeChallenge13
+  {
+    public static bool ValidateBrackets(string syntax)
     {
-        public static bool ValidateBrackets(string syntax)
+      Stack Brackets = new Stack();
+
+      foreach (char c in syntax)
+      {
+        if (c == '[' || c == '(' || c == '{')
         {
-            return false;
+          Brackets.Push(c);
         }
+        else if (c == ']' || c == ')' || c == '}')
+        {
+          if (Brackets.Count == 0)
+          {
+            return false;
+          }
+
+          char top = (char)Brackets.Peek();
+
+          if ((c == ']' && top == '[') || (c == ')' && top == '(') || (c == '}' && top == '{'))
+          {
+            Brackets.Pop();
+          }
+          else
+          {
+            return false;
+          }
+        }
+      }
+
+      return Brackets.Count == 0;
     }
+  }
 }
